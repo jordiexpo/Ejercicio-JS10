bba0482346df8b6010e6615430da9b27eb15750c
Jordi Exposito <exjoru220806@gmail.com>
2024-04-08 09:36:08 +0200

    Ejercicio JS10

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let numero = 1; /*Primero definimos que la variable numero que sera igual a 1 */
+let numero = 1;/*Primero definimos que la variable numero que sera igual a 1 */
 let i = 0;/*Luego definimos la variable i como 0 */
-do{
+do{ /*Con el do while queremos crear un bucle */
   if(i === 0) {
    i++;
    numero--;
@@ -9,4 +9,4 @@ do{
    numero++;
    console.log(numero);
   }
-} while (numero < 5);
+} while (numero < 5);/*Aqui indicamos que el bucle seguira siempre que la variable numero sea menor a 5, eso quiere decir que al llegar a ese numero se parara*/
